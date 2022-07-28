5b30f659b2f5690a825c1f5a4c2ade859fc434ad
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-28 15:36:01 +0200

    palindrome

---
+++ palindromo/app.js
@@ -0,0 +1,12 @@
+
+
+const word = prompt('inserisci una parola');
+const wordArray = word.split('');
+const wordArrayReversed = wordArray.reverse();
+
+console.log(wordArray, wordArrayReversed);
+
+function isPalindrome(word) {
+    if (wordArray === wordArrayReversed);
+    return true
+}
